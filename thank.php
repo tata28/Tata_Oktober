8c4123a854b77fd53c72dd2f289be53dbf0ebddc
Pacquita <pacquitamtj@gmail.com>
2019-10-31 19:37:34 +0700

    thank1

---
+++ thank.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+
+	<?php
+	header("Refresh:5; url=index.php");
+	?>
+<center style="font-size: 13pt">
+Thank you for your time,  <?php echo $_POST["Username"]; ?><br>
+Have a nice day !
+</center>
+</body>
+</html>
